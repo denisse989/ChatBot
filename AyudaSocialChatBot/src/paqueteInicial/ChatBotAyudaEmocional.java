ad89f8963198084422f165eb2bb375581d942bf6
Llubitza-Linares <llubylinares@gmail.com>
2022-04-03 09:25:04 -0400

    segundo commit

--- AyudaSocialChatBot/src/paqueteInicial/ChatBotAyudaEmocional.java
+++ AyudaSocialChatBot/src/paqueteInicial/ChatBotAyudaEmocional.java
@@ -22,6 +22,23 @@ class Chatbot extends JFrame {
 	private JLabel l = new JLabel();
 
 	Chatbot() {
+		
+		JFrame f=new JFram ();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+		f.setResizable(false);
+		f.setLayout(null);
+		f.setSize(400,400);
+		f.getContentPane().setBackground(Color.cyan);
+		f.setTitle("ChatBot");
+		f.add(ca);
+		f.add(cd);
+		f.add(b);
+		b.add(l);
+		ca.setSize(300,310);
+		ca.setLocation(1,1);
+		ca.setBackground(Color.BLACK);
+		b.setSize(400,20);
 
 	}
 
