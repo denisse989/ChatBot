a821831f7dee6960ba3b81759239451a294b7179
Ricardo Valencia <ignacio.valencia.sauma@gmail.com>
2022-04-02 21:35:49 -0400

    inicializacion de codigo

--- AyudaSocialChatBot/src/paqueteInicial/ChatBotAyudaEmocional.java
+++ AyudaSocialChatBot/src/paqueteInicial/ChatBotAyudaEmocional.java
@@ -1,10 +1,36 @@
 package paqueteInicial;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+class Chatbot extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextArea ca = new JTextArea();
+	private JTextField cf = new JTextField();
+	private JButton b = new JButton();
+	private JLabel l = new JLabel();
+
+	Chatbot() {
+
+	}
+
+}
+
 public class ChatBotAyudaEmocional {
 
 	public static void main(String[] args) {
-		
-
+		new Chatbot();
 	}
 
 }
